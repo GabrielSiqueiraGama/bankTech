b2bc3c284ab8a903d9497e7f04aa7e85dc9b84ce
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2026-01-14 22:59:29 -0300

    create: :tada: first commit, docker & MySQL

---
+++ src/main/java/com/example/bankTech/BankTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankTech/BankTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
