af0d909da0e5d48f9e221a780a6ac9d24b2484cf
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2026-01-20 23:10:58 -0300

    tests: :rotating_light: Initializing tests

---
+++ src/test/java/com/example/bankTech/repositories/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.bankTech.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+
+    @Test
+    void findUserByDocument() {
+    }
+
+
+
+
+}
+
